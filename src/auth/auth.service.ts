3cc8b9f201bfa7de1e559751bae4cde86156cffd
masoomeh ghorbani <masoomeh77ghorbani98@gmail.com>
2025-05-25 18:11:22 +0330

    Add: Signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signin() {
     return 'signin';
   }
 
-  signup(dto:AuthDto) {
-    return 'signup';
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          // P2002 is for duplicate content
+          throw new ForbiddenException('Credential is taken!');
+        }
+        throw error;
+      }
+    }
   }
 }
