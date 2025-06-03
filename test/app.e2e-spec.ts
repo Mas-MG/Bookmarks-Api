8c0c9504799cc1c9d03c8b9dfac2d8e689b5e5ee
masoomeh ghorbani <masoomeh77ghorbani98@gmail.com>
2025-06-03 13:11:58 +0330

    Add: Get me (Get current user)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -64,8 +64,22 @@ describe('App e2e', () => {
           .spec()
           .post('/auth/signin')
           .withBody(dto)
-          .expectStatus(200);
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
       });
     });
   });
+
+  describe('User', () => {
+    describe('Get me', () => {
+      it('should get current user',()=>{
+        return pactum.spec().get('/user/me').withHeaders({
+          Authorization:'Bearer $S{userAt}'
+        }).expectStatus(200)
+      })
+    });
+    
+    
+  });
+  
 });
